acb6fbc62d92ef870a2a09a9cdbf9ef1f6c64730
kbdbvgm <kbd.bvgm@gmail.com>
2023-12-28 18:03:27 +0300

    Correct s.Duration to s.Duration.Hours() in meanSpeed() for Swimming struct

--- main.go
+++ main.go
@@ -156,7 +156,7 @@ type Swimming struct {
 // длина_бассейна * количество_пересечений / м_в_км / продолжительность_тренировки
 // Это переопределенный метод meanSpeed() из Training.
 func (s Swimming) meanSpeed() float64 {
-	if s.Duration == 0 {
+	if s.Duration.Hours() == 0 {
 		return 0
 	}
 	return float64(s.LengthPool) * float64(s.CountPool) / MInKm / s.Duration.Hours()
